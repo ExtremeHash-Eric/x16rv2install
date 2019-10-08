129936630697cf878815d5559f61fcd826f38224
ExtremeHash-Eric <56253873+ExtremeHash-Eric@users.noreply.github.com>
2019-10-08 02:56:30 +0200

    Update x16rv2install.sh

--- x16rv2install.sh
+++ x16rv2install.sh
@@ -43,8 +43,8 @@ sudo ~/x16/NextJtag-2.4.0-Linux-x86_64/bin/nextjtag -a -c
 sudo ~/x16/NextJtag-2.4.0-Linux-x86_64/bin/nextjtag -m -a --set-jtag-freq 30000000 -b ~/x16/bitstreams/top.bit
 
 cd ~/x16/as_xdma_driver/driver
-sudo make clean
-sudo make
+make clean
+make
 sudo ~/x16/rescan.sh
 sudo ~/x16/as_xdma_driver/driver/load_driver.sh
 sudo ~/x16/rescan.sh
