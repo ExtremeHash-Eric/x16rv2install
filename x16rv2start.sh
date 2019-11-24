124206cd3a75ba805e8b3493c89ff049423c8d9d
ExtremeHash-Eric <56253873+ExtremeHash-Eric@users.noreply.github.com>
2019-11-24 15:46:20 +0100

    Update x16rv2start.sh

--- x16rv2start.sh
+++ x16rv2start.sh
@@ -15,8 +15,8 @@ sudo systemctl disable minerator
 
 cd x16
 
-sudo ~/x16/NextJtag-2.4.0-Linux-x86_64/bin/nextjtag -a -c
-sudo ~/x16/NextJtag-2.4.0-Linux-x86_64/bin/nextjtag -m -a --set-jtag-freq 30000000 -b ~/x16/bitstreams/top.bit
+sudo ~/x16/NextJtag-2.5.0-Linux-x86_64/bin/nextjtag -a -c
+sudo ~/x16/NextJtag-2.5.0-Linux-x86_64/bin/nextjtag -m -a --set-jtag-freq 30000000 -b ~/x16/bitstreams/top.bit
 
 cd ~/x16/as_xdma_driver/driver
 sudo make clean
